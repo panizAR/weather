e18b18a0314536464b73cf50003cb3d38d02fe66
p.a.r <p.a.r.mygit@gmail.com>
2023-06-18 16:32:03 +0330

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Search() {
+  let [City, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let Url = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${apiKey}&units=metric`;
+    axios.get(Url).then(displayWeather);
+  }
+  function cityName(event) {
+    setCity(event.target.value);
+  }
+  if (loaded) {
+    return (
+      <div className="center">
+        <form onSubmit={submit}>
+          <input
+            type="text"
+            placeholder="search for a city"
+            onChange={cityName}
+          />
+          <input type="submit" value="search" />
+        </form>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="center">
+        <form onSubmit={submit}>
+          <input
+            type="text"
+            placeholder="search for a city"
+            onChange={cityName}
+          />
+          <input type="submit" value="search" />
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
